f56817364567d3e0550788c33c429367a00af073
Saiprasath13021999 <saiprasath777@gmail.com>
2023-07-03 14:16:30 +0530

    Making like button work

--- src/FooterRight.js
+++ src/FooterRight.js
@@ -6,37 +6,37 @@ import ShareIcon from './icons/Share'
 import './FooterRight.css'
 
 function VideoSidebar({ likes, shares, messages }) {
-	// TODO: Add code here
+	// do something
+	const [liked, setLiked] = useState(false)
+    const [likeCount, setLikeCount] = useState(likes);
+
 	return (
-        <div className='footer-right'>
-            <div className='sidebar-icon'>
-                <FavoriteBorderIcon style= {{ 
-                    width : "40px",
-                    height : "40px"
-                }}
-                />
-                <p>{likes}</p>   
-            </div>
-            <div className='sidebar-icon record-be;low'>
-                <shareIcon style ={{
-                    width: "40px",
-                    height: "40px"
-                }}
-                />
-                <p>{messages}</p>
-            </div>
-            <div className="sidebar-icon record-below">
-		        <ShareIcon style={{ 
-                    width: '40px', 
-                    height: '40px' }} />
-		        <p>{shares}</p>
-	        </div>
-	        <div className="sidebar-icon record">
-		        <img src="https://static.thenounproject.com/png/934821-200.png" />
-	        </div>
-        
-        </div>
-    )
+		<div className="footer-right">
+			<div className="sidebar-icon">
+				{liked ? <FavoriteIcon style={{ width: '40px', height: '40px' }}/> :                   <FavoriteBorderIcon
+					style={{ width: '40px', height: '40px' }}
+					onClick={() => {
+						// do something
+                        setLiked(true);
+                        setLikeCount((likes) => likes + 1);
+					}}
+				/>}
+
+				<p>{likeCount}</p>
+			</div>
+			<div className="sidebar-icon">
+				<MessageIcon style={{ width: '40px', height: '40px' }} />
+				<p>{messages}</p>
+			</div>
+			<div className="sidebar-icon record-below">
+				<ShareIcon style={{ width: '40px', height: '40px' }} />
+				<p>{shares}</p>
+			</div>
+			<div className="sidebar-icon record">
+				<img src="https://static.thenounproject.com/png/934821-200.png" />
+			</div>
+		</div>
+	)
 }
 
 export default VideoSidebar
